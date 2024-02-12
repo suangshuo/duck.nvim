eb063aa9e6be1ece3249420980718a3a5fc34463
suangshuo <suangshuo_ok@outlook.com>
2024-02-12 11:55:27 +0800

    hello

--- lua/duck/init.lua
+++ lua/duck/init.lua
@@ -1,4 +1,9 @@
-local M={}
-local M.setup=function ()
-    
+local config=require("duck.config")
+
+local  setup=function(new_config)
+    config.config = vim.tbl_deep_extend("force", config.config, new_config or {})
 end
+
+return {
+    setup = setup
+}
