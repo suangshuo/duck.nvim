61c14bad7613cf7b81597e572312ab8df2916410
suangshuo <suangshuo_ok@outlook.com>
2024-02-26 19:36:02 +0800

    完善tools功能

--- lua/duck/init.lua
+++ lua/duck/init.lua
@@ -1,14 +1,14 @@
 local config=require("duck.config")
 local core=require("duck.do")
+local conf=config.config
 
 local config_if=function()
-    local conf=config.config
     if(conf.tools.cta)then
         core.ch_to_ascii()
     end
 end
 local  setup=function(new_config)
-    config.config = vim.tbl_deep_extend("force", config.config, new_config or {})
+    conf = vim.tbl_deep_extend("force", conf, new_config or {})
     core.todo()
     config_if();
 end
